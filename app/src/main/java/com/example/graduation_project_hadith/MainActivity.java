42998ce5a5832d63190d74cab0a3627bc431df27
TasneemMohammad <tasneemmohamed504@gmail.com>
2021-11-24 13:40:00 +0200

    Welcome to the first step of the Graduation Project .

---
+++ app/src/main/java/com/example/graduation_project_hadith/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.graduation_project_hadith;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
